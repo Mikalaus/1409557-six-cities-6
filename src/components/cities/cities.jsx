070ee19e86456095c37c5f114c8ab35207573cc2
Mikle <mmigalenko@mail.ru>
2021-04-07 12:36:48 +0300

    убрана ошибка линтера

--- src/components/cities/cities.jsx
+++ src/components/cities/cities.jsx
@@ -60,7 +60,19 @@ Cities.propTypes = {
         zoom: PropTypes.number.isRequired
       }
   ),
-  cityName: PropTypes.string.isRequired
+  cityName: PropTypes.string.isRequired,
+  offers: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        isFavorite: PropTypes.bool.isRequired,
+        isPremium: PropTypes.bool.isRequired,
+        previewImage: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        rating: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired
+      })
+  ),
 };
 
 const mapStateToProps = (state) => {
